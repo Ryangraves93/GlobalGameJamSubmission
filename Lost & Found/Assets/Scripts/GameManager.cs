b7296901a7265404298729004a33953f5e158a78
Ryangraves93 <ryangraves1993@gmail.com>
2021-01-30 13:22:58 +0000

    Added enemyBehaviour script and game manager class

---
+++ Lost & Found/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+
+    public float transitionTime = 1f;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    void MoveCamera()
+    {
+
+    }
+
+    void LoadLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
